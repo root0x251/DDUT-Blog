52eeb997081241cf83af23d11160bbec087bb9af
root0x251 <bortn@SZEMO.RU>
2022-06-16 13:38:30 +0300

    Init clear project

---
+++ src/main/java/com/bortn/ddutBlog/DdutBlogApplication.java
@@ -0,0 +1,13 @@
+package com.bortn.ddutBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdutBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdutBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bortn/ddutBlog/DdutBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bortn.ddutBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdutBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
